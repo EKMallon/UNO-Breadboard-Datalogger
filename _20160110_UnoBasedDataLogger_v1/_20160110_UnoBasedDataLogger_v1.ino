b152ab3cc3c36757b45d9e3753820a751e31223b
The Cave Pearl Project <EKMallon@users.noreply.github.com>
2018-02-25 15:15:27 -0600

    Add A0 analog read
    Add a bit of code to demonstrate how you would add an another sensor reading to the script

--- _20160110_UnoBasedDataLogger_v1/_20160110_UnoBasedDataLogger_v1.ino
+++ _20160110_UnoBasedDataLogger_v1/_20160110_UnoBasedDataLogger_v1.ino
@@ -1,5 +1,5 @@
-/* A very simple RTC triggered datalogger script. Note that I have replaced the
-analog pin reads in Tom’s Igoes starter code at //https://www.arduino.cc/en/Tutorial/Datalogger
+/* A basic RTC triggered datalogger script. Note that I have replaced the
+analog pin reads in Tom’s Igoes starter code from //https://www.arduino.cc/en/Tutorial/Datalogger
 with DS3231 I2C register reading & the delay has been replaced with sleep & RTC interrupt alarms
 */
 #include <SD.h>  // I much prefer SdFat.h by Greiman over the old SD.h library used here
@@ -7,36 +7,36 @@ with DS3231 I2C register reading & the delay has been replaced with sleep & RTC 
 const int chipSelect = 10;    //CS moved to pin 10 on the arduino
 #include <Wire.h>
 #include "LowPower.h"     // from https://github.com/rocketscream/Low-Power
-#include <RTClib.h>       // library from   https://github.com/MrAlvin/RTClib
+#include <RTClib.h>       // library from https://github.com/MrAlvin/RTClib  there are many other DS3231 libs availiable
 RTC_DS3231 RTC;
 // creates an RTC object in the code
-//variables for reading the RTC time & handling the INT(0) interrupt it generates
+// variables for reading the RTC time & handling the INT(0) interrupt it generates
 #define DS3231_I2C_ADDRESS 0x68
 int RTC_INTERRUPT_PIN = 2;
 byte Alarmhour;
 byte Alarmminute;
 byte Alarmday;
 char CycleTimeStamp[ ] = "0000/00/00,00:00"; //16 ascii characters (without seconds)
-#define SampleIntervalMinutes 1
-/*
-// CHANGE SampleIntervalMinutes to the number of minutes you want between samples! Whole numbers 1-59 only
-// NOTE yes the RTClib will do down to one second intervals with the right code
-// HOWEVER if you cut power to an SD card while it
-// is writing information you destroy the data file on the card….
-// so how do you solve that problem if you start sampling too frequently?
-*/
+#define SampleIntervalMinutes 1  // Whole numbers 1-30 only, must be a divisor of 60
+// CHANGE SampleIntervalMinutes to the number of minutes you want between samples!
+// NOTE: the RTC will go down to one second alarm intervals with additonal code
+
 volatile boolean clockInterrupt = false;  
 //this flag is set to true when the RTC interrupt handler is executed
 
 const char codebuild[] PROGMEM = __FILE__;  // loads the compiled source code directory & filename into a varaible
-const char header[] PROGMEM = "Timestamp,RTC temp, add more as needed"; //this gets written to datalog.txt in setup
+const char header[] PROGMEM = "Timestamp,RTC temp(C),Analog(A0),Add more headers here"; //gets written to second line datalog.txt in setup
 
 //variables for reading the DS3231 RTC temperature register
 float temp3231;
 byte tMSB = 0;
 byte tLSB = 0;
 
-//indicator LED pins
+//variables for analog pin reading
+int analogPin = 3;
+int valA0 = 0;
+
+//indicator LED pins - change to suit your connections
 int RED_PIN = 4;
 int GREEN_PIN = 5;
 int BLUE_PIN = 6; 
@@ -45,7 +45,7 @@ void setup() {
   
   // Setting the SPI pins high helps some sd cards go into sleep mode 
   // the following pullup resistors only needs to be enabled for the ProMini builds - not the UNO loggers
-  pinMode(chipSelect, OUTPUT); digitalWrite(chipSelect, HIGH); //Always pullup the CS pin with the SD library
+  pinMode(chipSelect, OUTPUT); digitalWrite(chipSelect, HIGH); //ALWAYS pullup the ChipSelect pin with the SD library
   //and you may need to pullup MOSI/MISO, usually MOSIpin=11, and MISOpin=12
   //pinMode(MOSIpin, OUTPUT); digitalWrite(MOSIpin, HIGH); //pullup the MOSI pin
   //pinMode(MISOpin, INPUT); digitalWrite(MISOpin, HIGH);  //pullup the MISO pin
@@ -86,7 +86,8 @@ void setup() {
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, HIGH); // startup with green led lit
   digitalWrite(BLUE_PIN, LOW);
-} // end of setup
+}
+//============end of setup=================
 
 void loop() {
   //—–This part reads the time and disables the RTC alarm
@@ -126,21 +127,28 @@ Serial.print(F(". TEMPERATURE from RTC is: "));
 Serial.print(temp3231);
 Serial.println(F(" Fahrenheit"));
 
-// You could read in other variables here …like the analog pins, etc,
+// You could read in other variables here …like the analog pins, I2C sensors, etc
 // and just add them to the dataString before you write it to the file
+valA0 = analogRead(analogPin); 
+// for stand-alone ProMini loggers, I monitor the main battery voltage (which is > Aref)
+// with a voltage divider: RawBattery - 10MΩ - A0 - 3.3MΩ - GND
+// with a 104 ceramic capacitor accross the 3.3MΩ resistor to enable the ADC to read the high impedance resistors
+// Then calculate: float batteryVoltage = float((analogRead(A0)/ 255.75)*3.3);
 
-//——– concatenate data into a string ———-
+//=========concatenate data into a string =====================
 // Add each piece of information to the string that gets written to the SD card with:dataFile.println(dataString);
 String dataString = ""; //this line simply erases the string
-dataString += CycleTimeStamp;
-dataString += ", ";     //puts a comma between the two bits of data
+dataString = dataString + CycleTimeStamp;
+dataString = dataString + ", ";     //separate data with a comma so they can be imported to separate collumns in a spreadsheet
 dataString = dataString + String(temp3231);  //this only gives you two decimal places
-//if you want more numbers after the decimal place replace the above line with
+//With floating point nubmers, if you want more digits after the decimal place replace the above line with these three:
 //char buff[10];
 //dtostrf(rtc_TEMP_degC, 4, 4, buff);  //4 is mininum width, second number is #digits after the decimal
 //dataString = dataString + buff;
+dataString = dataString + ", "; 
+dataString = dataString + String(valA0);
 
-//——– Now write the data to the SD card ——–
+//========== Now write the data to the SD card ===========
 // open the file. note that only one file can be open at a time,
 // so you have to close this one before opening another.
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
@@ -157,10 +165,7 @@ dataString = dataString + String(temp3231);  //this only gives you two decimal p
     Serial.println(F("error opening datalog.txt"));
   }
   
-// delay(10000);
-// instead of using the delay we will use RTC interrupted sleeps
-
-//——– Set the next alarm time ————–
+//============Set the next alarm time =============
 Alarmhour = now.hour();
 Alarmminute = now.minute() + SampleIntervalMinutes;
 Alarmday = now.day();
@@ -184,15 +189,13 @@ if (RTC.checkAlarmEnabled(1)) {
   Serial.print(F(" Going to sleep for : "));
   Serial.print(SampleIntervalMinutes);
   Serial.println(F(" minutes"));
-  Serial.println();Serial.flush();        //just adds a carriage return
+  Serial.println();Serial.flush();//adds a carriage return & waits for buffer to empty
 }
-delay(100); //this delay is only here so we can see the LED’s it is totally optional!
-  //otherwise the entire loop would execute too fast for us to see it!
+
+  delay(25); //this optional delay is only here so we can see the LED’s otherwise the entire loop executes so fast you might not see it.
   digitalWrite(GREEN_PIN, LOW);
-  digitalWrite(RED_PIN, HIGH);
-  // Turn on red led as our indicator that the Arduino is sleeping.
-  // Note: Normally you would NOT leave an LED on like this during sleep! This is just so you can see what is going on..
-  
+  // Note: Normally you would NOT leave a red indicator LED on during sleep! This is just so you can see when your logger is sleeping, & when it's awake
+  digitalWrite(RED_PIN, HIGH);  // Turn on red led as our indicator that the Arduino is sleeping. Remove this before deployment.
   //——– sleep and wait for next RTC alarm ————–
   // Enable interrupt on pin2 & attach it to rtcISR function:
   attachInterrupt(0, rtcISR, LOW);
@@ -202,10 +205,10 @@ delay(100); //this delay is only here so we can see the LED’s it is totally op
   detachInterrupt(0); // immediately disable the interrupt on waking
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, HIGH); //Interupt woke processor, turn on green led
+}
+//================ END of the MAIN LOOP ===============
 
-}// this is the END of the MAIN LOOP
-  // This is the Interrupt subroutine that only executes when the rtc alarm goes off
-
+// This is the Interrupt subroutine that only executes when the RTC alarm goes off
 void rtcISR() {
     clockInterrupt = true;
   }
